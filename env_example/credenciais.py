874528e0c07d3cedf11fcff697c92013c5332c0f
Victor Lima <victor_lima27@hotmail.com>
2024-10-30 21:43:09 -0300

    Add Env Example

---
+++ env_example/credenciais.py
@@ -0,0 +1,9 @@
+SGBD_cred = 'mariadb+mysqlconnector'
+#SGBD_cred = 'mysql+mysqlconnector'
+usuario_cred = 'root'
+senha_cred = ''
+servidor_cred = 'localhost'
+database_cred = 'ged_contabilidade'
+
+
+SECRET_KEY_cred = 'Control_GED'
